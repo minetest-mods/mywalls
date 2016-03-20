78f0917b5c7f3b3b99190a7ee48e09a68706a5e7
DonBatman <don@serfdon.com>
2016-03-20 10:16:34 -0700

    First upload

---
+++ init.lua
@@ -0,0 +1,22 @@
+walls.register("mywalls:brick", "Brick Wall", "default_brick.png",
+		"default:brick", default.node_sound_stone_defaults())
+
+walls.register("mywalls:sandstone", "Sandstone Wall", "default_sandstone.png",
+		"default:sandstone", default.node_sound_stone_defaults())
+
+walls.register("mywalls:sandstone_brick", "Sandstone Brick Wall", "default_sandstone_brick.png",
+		"default:sandstonebrick", default.node_sound_stone_defaults())
+
+walls.register("mywalls:stone", "Stone Wall", "default_stone.png",
+		"default:stone", default.node_sound_stone_defaults())
+
+walls.register("mywalls:stone_brick", "Stone Brick Wall", "default_stone_brick.png",
+		"default:stonebrick", default.node_sound_stone_defaults())
+
+walls.register("mywalls:desert_stone", "Desert Stone Wall", "default_desert_stone.png",
+		"default:desert_stone", default.node_sound_stone_defaults())
+
+walls.register("mywalls:desrt_stone_brick", "Desert Stone Brick Wall", "default_desert_stone_brick.png",
+		"default:desert_stonebrick", default.node_sound_stone_defaults())
+
+
