107ac0e192f560a30834affcac363ae4753d3689
Alexander Chibrikin <alek13.me@gmail.com>
2022-11-29 04:34:48 +0300

    add additional walls; add tranlations; improve `readme.md`

--- init.lua
+++ init.lua
@@ -1,20 +1,64 @@
-walls.register(":walls:brick", "Brick Wall", "default_brick.png",
+local S = minetest.get_translator("mywalls")
+
+walls.register(":walls:brick", S("Brick Wall"), "default_brick.png",
 		"default:brick", default.node_sound_stone_defaults())
 
-walls.register(":walls:sandstone", "Sandstone Wall", "default_sandstone.png",
+
+walls.register(":walls:sandstone", S("Sandstone Wall"), "default_sandstone.png",
 		"default:sandstone", default.node_sound_stone_defaults())
 
-walls.register(":walls:sandstone_brick", "Sandstone Brick Wall", "default_sandstone_brick.png",
+walls.register(":walls:sandstone_brick", S("Sandstone Brick Wall"), "default_sandstone_brick.png",
 		"default:sandstonebrick", default.node_sound_stone_defaults())
 
-walls.register(":walls:stone", "Stone Wall", "default_stone.png",
+walls.register(":walls:sandstone_block", S("Sandstone Block Wall"), "default_sandstone_block.png",
+		"default:sandstone_block", default.node_sound_stone_defaults())
+
+
+walls.register(":walls:stone", S("Stone Wall"), "default_stone.png",
 		"default:stone", default.node_sound_stone_defaults())
 
-walls.register(":walls:stone_brick", "Stone Brick Wall", "default_stone_brick.png",
+walls.register(":walls:stone_brick", S("Stone Brick Wall"), "default_stone_brick.png",
 		"default:stonebrick", default.node_sound_stone_defaults())
 
-walls.register(":walls:desert_stone", "Desert Stone Wall", "default_desert_stone.png",
+walls.register(":walls:stone_block", S("Stone Block Wall"), "default_stone_block.png",
+		"default:stone_block", default.node_sound_stone_defaults())
+
+
+walls.register(":walls:desert_stone", S("Desert Stone Wall"), "default_desert_stone.png",
 		"default:desert_stone", default.node_sound_stone_defaults())
 
-walls.register(":walls:desert_stonebrick", "Desert Stone Brick Wall", "default_desert_stone_brick.png",
+walls.register(":walls:desert_stonebrick", S("Desert Stone Brick Wall"), "default_desert_stone_brick.png",
 		"default:desert_stonebrick", default.node_sound_stone_defaults())
+
+walls.register(":walls:desert_stone_block", S("Desert Stone Block Wall"), "default_desert_stone_block.png",
+		"default:desert_stone_block", default.node_sound_stone_defaults())
+
+
+walls.register(":walls:desert_sandstone", S("Desert Sandstone Wall"), "default_desert_sandstone.png",
+		"default:desert_sandstone", default.node_sound_stone_defaults())
+
+walls.register(":walls:desert_sandstone_brick", S("Desert Sandstone Brick Wall"), "default_desert_sandstone_brick.png",
+		"default:desert_sandstone_brick", default.node_sound_stone_defaults())
+
+walls.register(":walls:desert_sandstone_block", S("Desert Sandstone Block Wall"), "default_desert_sandstone_block.png",
+		"default:desert_sandstone_block", default.node_sound_stone_defaults())
+
+
+walls.register(":walls:silver_sandstone", S("Silver Sandstone Wall"), "default_silver_sandstone.png",
+		"default:silver_sandstone", default.node_sound_stone_defaults())
+
+walls.register(":walls:silver_sandstone_brick", S("Silver Sandstone Brick Wall"), "default_silver_sandstone_brick.png",
+		"default:silver_sandstone_brick", default.node_sound_stone_defaults())
+
+walls.register(":walls:silver_sandstone_block", S("Silver Sandstone Block Wall"), "default_silver_sandstone_block.png",
+		"default:silver_sandstone_block", default.node_sound_stone_defaults())
+
+
+walls.register(":walls:obsidian", S("Obsidian Wall"), "default_obsidian.png",
+		"default:obsidian", default.node_sound_stone_defaults())
+
+walls.register(":walls:obsidian_brick", S("Obsidian Brick Wall"), "default_obsidian_brick.png",
+		"default:obsidian_brick", default.node_sound_stone_defaults())
+
+walls.register(":walls:obsidian_block", S("Obsidian Block Wall"), "default_obsidian_block.png",
+		"default:obsidian_block", default.node_sound_stone_defaults())
